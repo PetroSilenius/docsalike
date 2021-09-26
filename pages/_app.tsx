08454453071ac30091319cd558b525487772f635
Petro Silenius <petro.silenius@gmail.com>
2021-09-26 13:04:20 +0300

    Add maskable icon, robots.txt and meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <Head>
         <title>Docsalike - A Google Docs clone</title>
+        <meta
+          name="description"
+          content="In docsalike users can create and edit rich text documents to take notes. They can access the service from any of their devices."
+        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
