fed1c8aa8b5a0c6e19a4a67899b27b122c716f6e
Petro Silenius <petro.silenius@gmail.com>
2021-09-26 13:05:00 +0300

    Install next-pwa and wrap next config withPWA func

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
-}
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
